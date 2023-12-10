6d5c4c0f423ddd15fcd4a1ab5ab32ed423503345
D0MHan <xlzlxkzk11@gmail.com>
2023-12-10 23:47:12 +0900

    hi

--- app/src/main/java/kr/ac/daelim/newweatherapp/MainActivity.kt
+++ app/src/main/java/kr/ac/daelim/newweatherapp/MainActivity.kt
@@ -4,37 +4,50 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.SearchView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import kr.ac.daelim.newweatherapp.databinding.ActivityMainBinding
-import retrofit2.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var isRefreshing = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        fetchWeatherData("seoul")
+        val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.swipeRefreshLayout)
+        swipeRefreshLayout.setOnRefreshListener {
+            if (!isRefreshing) {
+                isRefreshing = true
+                fetchWeatherData("Suwon")
+            }
+        }
+
         SearchCity()
+        fetchWeatherData("Suwon")
     }
 
     private fun SearchCity() {
         val searchView = binding.searchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 if (query != null) {
                     fetchWeatherData(query)
                 }
-
                 return true
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
                 return true
             }
-
         })
     }
 
@@ -45,25 +58,24 @@ class MainActivity : AppCompatActivity() {
             .build().create(ApiInterface::class.java)
 
         val response = retrofit.getWeatherData(cityName, "91badfada80424a85d04e28c349d62d4", "metric")
-        response.enqueue(object : Callback<WeatherApp>{
+        response.enqueue(object : Callback<WeatherApp> {
             override fun onResponse(call: Call<WeatherApp>, response: Response<WeatherApp>) {
                 val responseBody = response.body()
-                if (response.isSuccessful && responseBody != null){
-                    val temperature = responseBody.main.temp.toString()
+                if (response.isSuccessful && responseBody != null) {
+                    val temperature = responseBody.main.temp.toDouble()
                     val humidity = responseBody.main.humidity
                     val windSpeed = responseBody.wind.speed
                     val sunRise = responseBody.sys.sunrise.toLong()
                     val sunSet = responseBody.sys.sunset.toLong()
                     val seaLevel = responseBody.main.pressure
                     val condition = responseBody.weather.firstOrNull()?.main?: "unknown"
-                    val maxTemp = responseBody.main.temp_max
-                    val minTemp = responseBody.main.temp_min
+                    val maxTemp = responseBody.main.temp_max.toDouble()
+                    val minTemp = responseBody.main.temp_min.toDouble()
 
-
-                    binding.temp.text = "${temperature} °C"
+                    binding.temp.text = "${temperature.roundToInt()} °C"
                     binding.weather.text = condition
-                    binding.maxTemp.text = "최고 : ${maxTemp} °C"
-                    binding.minTemp.text = "최저 : ${minTemp} °C"
+                    binding.maxTemp.text = "최고 : ${maxTemp.roundToInt()} °C"
+                    binding.minTemp.text = "최저 : ${minTemp.roundToInt()} °C"
                     binding.humidity.text = "${humidity} %"
                     binding.windSpeed.text = "${windSpeed} m/s"
                     binding.sunrise.text = "${time(sunRise)}"
@@ -71,39 +83,20 @@ class MainActivity : AppCompatActivity() {
                     binding.sea.text = "${seaLevel} hPa"
                     binding.conditions.text = condition
 
-                    if (dayName(System.currentTimeMillis()) == "Sunday")
-                    {
+                    if (dayName(System.currentTimeMillis()) == "Sunday") {
                         binding.day.text = "일요일"
-                    }
-                    else if(dayName(System.currentTimeMillis()) == "Monday")
-                    {
+                    } else if (dayName(System.currentTimeMillis()) == "Monday") {
                         binding.day.text = "월요일"
-
-                    }
-                    else if(dayName(System.currentTimeMillis()) == "Tuesday")
-                    {
+                    } else if (dayName(System.currentTimeMillis()) == "Tuesday") {
                         binding.day.text = "화요일"
-
-                    }
-                    else if(dayName(System.currentTimeMillis()) == "Wednesday")
-                    {
+                    } else if (dayName(System.currentTimeMillis()) == "Wednesday") {
                         binding.day.text = "수요일"
-
-                    }
-                    else if(dayName(System.currentTimeMillis()) == "Thursday")
-                    {
+                    } else if (dayName(System.currentTimeMillis()) == "Thursday") {
                         binding.day.text = "목요일"
-
-                    }
-                    else if(dayName(System.currentTimeMillis()) == "Friday")
-                    {
+                    } else if (dayName(System.currentTimeMillis()) == "Friday") {
                         binding.day.text = "금요일"
-
-                    }
-                    else if(dayName(System.currentTimeMillis()) == "Saturday")
-                    {
+                    } else if (dayName(System.currentTimeMillis()) == "Saturday") {
                         binding.day.text = "토요일"
-
                     }
 
                     binding.date.text = date()
@@ -111,25 +104,32 @@ class MainActivity : AppCompatActivity() {
 
                     changeImagesWeaher(condition)
                 }
+                // 스와이프 리프레시가 완료되면 플래그를 다시 false로 설정
+                isRefreshing = false
+                val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.swipeRefreshLayout)
+                swipeRefreshLayout.isRefreshing = false
             }
 
             override fun onFailure(call: Call<WeatherApp>, t: Throwable) {
-                TODO("Not yet implemented")
+                // 스와이프 리프레시가 실패하더라도 플래그를 false로 설정
+                isRefreshing = false
+                val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.swipeRefreshLayout)
+                swipeRefreshLayout.isRefreshing = false
             }
-
         })
     }
 
     private fun changeImagesWeaher(conditions: String) {
         when (conditions) {
-            "Clear Sky", "Sunny", "Clear" -> {
-                binding.root.setBackgroundResource(R.drawable.sunny_background)
-                binding.lottieAnimationView.setAnimation(R.raw.sun)
-            }
 
             "Haze", "Partly Clouds", "Clouds", "Overcast", "Mist", "Foggy", "Fog" -> {
                 binding.root.setBackgroundResource(R.drawable.colud_background)
                 binding.lottieAnimationView.setAnimation(R.raw.cloud)
+            }
+
+            "Clear Sky", "Sunny", "Clear" -> {
+                binding.root.setBackgroundResource(R.drawable.sunny_background)
+                binding.lottieAnimationView.setAnimation(R.raw.sun)
             }
 
             "Light Rain", "Drizzle", "Moderate Rain", "Showers", "Heavy Rain", "Rain" -> {
@@ -140,30 +140,27 @@ class MainActivity : AppCompatActivity() {
             "Light Snow", "Moderate Snow", "Blizzard", "Heavy Snow", "Snow" -> {
                 binding.root.setBackgroundResource(R.drawable.snow_background)
                 binding.lottieAnimationView.setAnimation(R.raw.snow)
-            } else -> {
-            binding.root.setBackgroundResource(R.drawable.sunny_background)
-            binding.lottieAnimationView.setAnimation(R.raw.sun)
-        }
+            }
+            else -> {
+                binding.root.setBackgroundResource(R.drawable.sunny_background)
+                binding.lottieAnimationView.setAnimation(R.raw.sun)
+            }
         }
         binding.lottieAnimationView.playAnimation()
     }
 
     private fun time(timestamp: Long): String {
         val sdf = SimpleDateFormat("HH:MM", Locale.getDefault())
-
-        return  sdf.format((Date(timestamp*1000)))
+        return sdf.format((Date(timestamp * 1000)))
     }
 
     private fun date(): String {
         val sdf = SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault())
-
-        return  sdf.format((Date()))
+        return sdf.format((Date()))
     }
-
 
     fun dayName(timestamp: Long): String {
         val sdf = SimpleDateFormat("EEEE", Locale.getDefault())
-
-        return  sdf.format((Date()))
+        return sdf.format((Date()))
     }
 }
